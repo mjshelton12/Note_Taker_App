9479d4c16b521aaadab5484b099cfc916dae6743
MJ_Shelton <mjshelton12@comcast.net>
2022-08-25 16:09:11 -0700

    Changed port to allow for Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const { json } = require('express');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json())
